7a99139bae5e93d05806a283232e38aa4d759458
jisu_jeong <jisujeong@jisu-jeongui-MacBookPro.local>
2021-12-30 01:01:03 +0900

    commit

--- js/game_manager.js
+++ js/game_manager.js
@@ -15,7 +15,8 @@ function GameManager(size, InputManager, Actuator, StorageManager) {
 
 // Restart the game
 GameManager.prototype.restart = function () {
-  window.location.reload();
+  // window.location.reload();
+  $(id_adfit).load(window.location.href + "id_adfit");
   this.storageManager.clearGameState();
   this.actuator.continueGame(); // Clear the game won/lost message
   this.setup();
